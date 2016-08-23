34b2aa93a8123b2ce051ca40f0a82edc7aa51404
Shaquille <shaquille-etienne@hotmail.com>
2016-08-23 17:52:07 +0100

    Allowed the tank to take damage from the projectile and prints tank has died when the tank health has reached 0

--- 04_BattleTank/BattleTank/Source/BattleTank/Private/Tank.cpp
+++ 04_BattleTank/BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -10,3 +10,18 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 }
+
+float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser)
+{
+	int32 DamagePoints = FPlatformMath::RoundToInt(DamageAmount); //Convert floating point damage to int damage
+	int32 DamageToApply = FMath::Clamp(DamagePoints, 0, CurrentHealth);
+
+	CurrentHealth -= DamageToApply;
+	if (CurrentHealth <= 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank Has died"))
+	}
+	
+
+	return DamageToApply;
+}
